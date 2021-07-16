4474726d7729f6d93d9b80c7e99d072caa13a89b
Eugene Paul Badato <badatoeugenepaulm@gmail.com>
2021-07-16 22:03:20 +0800

    feat: added modal in homepage and implement black and white bg

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,4 +1,10 @@
 <template>
+	<transition name="fade" appear>
+		<div class="modal__backdrop" v-if="showModal"></div>
+	</transition>
+	<transition name="pop" appear>
+		<Modal :movie="movie" v-if="showModal" @closeModal="handleCloseModal" />
+	</transition>
 	<div class="home">
 		<!-- <Heading :movie="movie" /> -->
 		<div
@@ -27,16 +33,33 @@
 			</form>
 		</div>
 		<div className="fade-effect"></div>
-		<Discover :url="request.topPopular" title="What's Popular?" />
-		<Discover :url="request.trending" title="What's Trending?" />
-		<Discover :url="request.topRated" title="Top Rated" />
-		<Discover :url="request.topAction" title="Top Action" />
+		<Discover
+			:url="request.topPopular"
+			title="What's Popular?"
+			@showModal="handleShowModal($event)"
+		/>
+		<Discover
+			:url="request.trending"
+			title="What's Trending?"
+			@showModal="handleShowModal($event)"
+		/>
+		<Discover
+			:url="request.topRated"
+			title="Top Rated"
+			@showModal="handleShowModal($event)"
+		/>
+		<Discover
+			:url="request.topAction"
+			title="Top Action"
+			@showModal="handleShowModal($event)"
+		/>
 	</div>
 </template>
 
 <script>
 import request from "@/axios/request";
 import Discover from "@/views/home/Discover";
+import Modal from "@/components/Modal";
 import Heading from "@/views/home/Heading";
 import Spinner from "@/components/Spinner";
 import { ref } from "vue";
@@ -49,6 +72,7 @@ export default {
 		Discover,
 		Heading,
 		Spinner,
+		Modal,
 	},
 	computed: {
 		iSearch: function() {
@@ -65,11 +89,31 @@ export default {
 	setup() {
 		const router = useRouter();
 		const search = ref("");
+		const showModal = ref(false);
+		const movie = ref(null);
+
+		const handleShowModal = (movieEvent) => {
+			movie.value = movieEvent;
+			console.log("show modal", movieEvent);
+			showModal.value = true;
+		};
+
+		const handleCloseModal = () => {
+			movie.value = null;
+			showModal.value = false;
+		};
 
 		const handleSearch = () => {
 			router.push({ name: "browse_movies", query: { q: search.value } });
 		};
-		return { handleSearch, search };
+		return {
+			handleSearch,
+			search,
+			showModal,
+			handleCloseModal,
+			handleShowModal,
+			movie,
+		};
 	},
 };
 </script>
