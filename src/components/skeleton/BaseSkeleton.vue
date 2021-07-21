7a35c42895981d0c26cb268e0e8bf0268b855c28
Eugene Paul Badato <badatoeugenepaulm@gmail.com>
2021-07-21 17:36:49 +0800

    plugins: implemented loader plugins and customize it

---
+++ src/components/skeleton/BaseSkeleton.vue
@@ -0,0 +1,39 @@
+<template>
+	<div class="skeleton" :class="type"></div>
+</template>
+
+<script>
+export default {
+	name: "BaseSkeleton",
+	props: ["type"],
+};
+</script>
+
+<style lang="scss" scoped>
+.skeleton {
+	background-color: #ddd;
+	margin: 10px 0;
+	border-radius: 4px;
+
+	&__text {
+		width: 100%;
+		height: 12px;
+	}
+	&__title {
+		width: 50%;
+		height: 20px;
+		margin-bottom: 15px;
+	}
+
+	&__avatar {
+		width: 50%;
+		height: 20px;
+		margin-bottom: 15px;
+	}
+
+	&__poster {
+		width: 138px;
+		height: 170px;
+	}
+}
+</style>
