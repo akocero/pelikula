d7912788bcac0c7d1debab8f3b37038270fe6a4d
Eugene Paul Badato <badatoeugenepaulm@gmail.com>
2021-07-23 19:21:32 +0800

    fix: useBG updates background

--- src/composables/getBG.js
+++ src/composables/getBG.js
@@ -3,11 +3,8 @@ import { computed, ref } from "vue";
 const getBG = () => {
    const bgArray = ref([
 			"/vVpEOvdxVBP2aV166j5Xlvb5Cdc.jpg", // John wick
-			"/nNX6KhETd0UIKfZStltsyg2M3v0.jpg", // Apocalypto
-			"/TU9NIjwzjoKPwQHoHshkFcQUCG.jpg", // Parasite
-			"/194dso1hBwQEgIU3fgS7mXHtFAj.jpg", // city of god
          "/tM894AtE7UQTJEoQG6qF6mdfSUT.jpg", // guardians of the galaxy
-         "/70AV2Xx5FQYj20labp0EGdbjI6E.jpg",
+         "/70AV2Xx5FQYj20labp0EGdbjI6E.jpg", // wrath of man
 	]);
 
    const randomBG = computed(()=> {
