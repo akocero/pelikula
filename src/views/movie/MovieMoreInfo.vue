b2bed2c4dd3d681e36e5e6412e2cb58a77db3e29
Eugene Paul Badato <badatoeugenepaulm@gmail.com>
2021-08-17 23:36:33 +0800

    style: improved more info design

--- src/views/movie/MovieMoreInfo.vue
+++ src/views/movie/MovieMoreInfo.vue
@@ -1,40 +1,38 @@
 <template>
-	<div class="movie-details__more-info">
-		<ul>
-			<li>
-				<h5>Status</h5>
-				<span>{{ movie.status }}</span>
-			</li>
-			<li>
-				<h5>Release Date</h5>
-				<span>{{ movie.release_date }}</span>
-			</li>
-			<li>
-				<h5>Original Language</h5>
-				<span>{{
-					movie.original_language === "en"
-						? "English"
-						: movie.original_language
-				}}</span>
-			</li>
-			<li>
-				<h5>Budget</h5>
-				<span>{{ movie.budget }}</span>
-			</li>
-			<li>
-				<h5>Revenue</h5>
-				<span>{{ movie.revenue }}</span>
-			</li>
-			<li>
-				<h5>Popularity</h5>
-				<span>{{ movie.popularity }}</span>
-			</li>
-			<li>
-				<h5>Vote Count</h5>
-				<span>{{ movie.vote_count }}</span>
-			</li>
-		</ul>
-	</div>
+	<ul class="movie-details__additional-details">
+		<li>
+			<label>Status</label>
+			<span>{{ movie.status }}</span>
+		</li>
+		<li>
+			<label>Release Date</label>
+			<span>{{ movie.release_date }}</span>
+		</li>
+		<li>
+			<label>Original Language</label>
+			<span>{{
+				movie.original_language === "en"
+					? "English"
+					: movie.original_language
+			}}</span>
+		</li>
+		<li>
+			<label>Budget</label>
+			<span>{{ movie.budget }}</span>
+		</li>
+		<li>
+			<label>Revenue</label>
+			<span>{{ movie.revenue }}</span>
+		</li>
+		<li>
+			<label>Popularity</label>
+			<span>{{ movie.popularity }}</span>
+		</li>
+		<li>
+			<label>Vote Count</label>
+			<span>{{ movie.vote_count }}</span>
+		</li>
+	</ul>
 </template>
 
 <script>
